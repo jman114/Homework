73cf07f0d538a82b32e79256ea0d11d3b0e9031c
James McMinn <jmcminn@matchboxdesigngroup.com>
2022-02-03 22:27:38 -0600

    Pizza Place Homework

---
+++ WeekTwoHomework.js
@@ -0,0 +1,21 @@
+//Two Variables
+let pizzaPlace = "Big Ole Pizza Store";
+let numberOfToppings = 39;
+
+//Assigning Variables
+//pizzaPlace = "Big Ole Pizza Store";
+//numberOfToppings = 39;
+
+//Printing Variable Values
+console.log(pizzaPlace);
+console.log(numberOfToppings);
+
+//Printing Variable Type
+console.log(typeof pizzaPlace);
+console.log(typeof numberOfToppings);
+
+let pizzaSummary = `My pizza place is ${pizzaPlace} and we have ${numberOfToppings} toppings.`;
+console.log(pizzaSummary);
+
+let toppings = 10;
+toppings >= 10 ? console.log("A whole lot of pizza") : console.log("Quality, not quantity.");
