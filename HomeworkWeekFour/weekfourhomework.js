2fd6fbd5bc0f465f79ac4b870f1ba409af4ddcb7
James McMinn <jmcminn@matchboxdesigngroup.com>
2022-02-21 12:11:08 -0600

    Week 4 Homework minus the bonus

---
+++ HomeworkWeekFour/weekfourhomework.js
@@ -0,0 +1,9 @@
+// let d = new Date();
+// document.body.innerHTML = "<h2>Today's date is " + d + "</h2>";
+
+// const dateString = document.querySelector('#modified-me').innerText;
+// document.querySelector('modified-me');
+
+
+// const div = document.querySelector('modified-me');
+// div.innerText = new Date();
